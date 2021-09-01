bb34d744510adc7c4532b8667d04f63ffc2cf1c1
Alexander Milevski <alexander@linkurio.us>
2021-09-01 16:51:27 +0200

    Cleanup

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,6 @@
-declare type MouseWheelEvent = WheelEvent;
 import { NodeAttributesValue, EdgeAttributesValue } from '@linkurious/ogma';
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export declare interface Theme<ND = any, ED = any> {
   nodeAttributes?: NodeAttributesValue<ND, ED>;
   edgeAttributes?: EdgeAttributesValue<ED, ND>;
